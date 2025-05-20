3e13716c73775cde6305626181f7c01e1c17affa
Felix Burkhard <felix.burkhard@aitgmbh.de>
2025-05-20 18:35:18 +0200

    add clientconsumer

---
+++ ClientConsumer/Program.cs
@@ -0,0 +1,10 @@
+﻿using ProjectManager.Simulator;
+
+var httpClient = new HttpClient();
+
+var client = new ProjectManagerSimulatorClient("http://localhost:5147", httpClient);
+var allEstimates = await client.GetAllEstimatesAsync(null);
+foreach (var estimate in allEstimates)
+{
+    Console.WriteLine($"{estimate.EstimatedBy} - {estimate.EstimatedTimespan}");
+}
